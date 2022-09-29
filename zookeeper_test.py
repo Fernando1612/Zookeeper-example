3b7a4c8ebaed73a27919322af3ac2a373ddbba41
Fernando Maceda <fernando.patricio.maceda@gmail.com>
2022-09-28 19:00:05 -0500

    Added Unit Test

--- zookeeper_test.py
+++ zookeeper_test.py
@@ -3,7 +3,7 @@ from zookeeper import Ztree
 
 class TestZookeeper(unittest.TestCase):
 
-    def test_crear_znode(self):
+    def test_crear_znode_ephemero(self):
         tree = Ztree()
         tree.create('/node1', 'algo', True, True, 10, '/')
         self.assertEqual(tree.getData('/node1'), 'algo')
@@ -12,8 +12,42 @@ class TestZookeeper(unittest.TestCase):
         with self.assertRaises(Exception):
             tree = Ztree()
             tree.create('/node1/node2/node3', 'algo', True, True, 10, None)
+    
+    def test_crear_znode_regular(self):
+        tree = Ztree()
+        tree.create('/node1', 'algo', False, True, 10, '/')
+        self.assertEqual(tree.getData('/node1'), 'algo')
+    
+    def test_crear_znode_ephemero_inactivo(self):
+        tree = Ztree()
+        tree.create('/node1', 'algo', True, False, 10, '/')
+        self.assertEqual(tree.getData('/node1'), 'algo')
 
+    def test_borrar_znode_regular(self):
+        tree = Ztree()
+        tree.create('/node1','algo',False,True,10,'/')
+        tree.delete('/node1',0)
+        self.assertFalse(tree.exist('/node1'))
 
+    def test_borrar_znode_ephemero(self):
+        tree = Ztree()
+        tree.create('/node1','algo',True,True,10,'/')
+        tree.delete('/node1',0)
+        self.assertFalse(tree.exist('/node1'))
+
+    def test_borrar_znode_ephemero_inactivo(self):
+        tree = Ztree()
+        tree.create('/node1','algo',True,False,10,'/')
+        tree.delete('/node1',0)
+        self.assertFalse(tree.exist('/node1'))
+
+    def test_cambiar_datos(self):
+        tree = Ztree()
+        tree.create('/n','algo',True,True,10,'/')
+        tree.setData('/n','algo1')
+        self.assertEqual(tree.getData('/n'),'algo1')
+
+        
 if __name__ == '__main__':
     unittest.main()
 
